ef1271b8ba85a51ce4baf65fda3a1658871b1474
mohamed-saad <mahameds19999@gmail.com>
2019-02-22 19:13:39 +0200

    init the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SpecialsComponent } from './components/specials/specials.component';
+import { TeamComponent } from './components/team/team.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { TestimonialsComponent } from './components/slider/testimonials/testimonials.component';
+import { CompaniesComponent } from './components/slider/companies/companies.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { FactsComponent } from './components/facts/facts.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DataComponent } from './components/footer/data/data.component';
+import { ContactComponent } from './components/footer/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProjectsComponent,
+    ServicesComponent,
+    SpecialsComponent,
+    TeamComponent,
+    SliderComponent,
+    TestimonialsComponent,
+    CompaniesComponent,
+    PostsComponent,
+    FactsComponent,
+    FooterComponent,
+    DataComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
